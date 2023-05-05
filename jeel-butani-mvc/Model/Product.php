bbfb93f81b4de2cf49885370f09a060c9d62aa27
JEELBUTANI <68411725+JEELBUTANI@users.noreply.github.com>
2023-05-05 09:35:55 +0530

    Add files via upload

--- jeel-butani-mvc/Model/Product.php
+++ jeel-butani-mvc/Model/Product.php
@@ -1,13 +1,23 @@
-<?php 
+<?php
 
-class Model_Product extends Model_Core_Table;
+class Model_Product extends Model_Core_Table
 {
-	
-	function __construct()
+	const STATUS_ACTIVE = 1;
+    const STATUS_ACTIVE_LBL = 'Active';
+    const STATUS_INACTIVE =  2;
+    const STATUS_INACTIVE_LBL = 'Inactive';
+    const STATUS_DEFAULT= 1;
+
+	public function getStatusOptions()
 	{
-		parent::__construct();
-		$this->setTableName('product')->setPrimaryKey('product_id');
+	  return [self::STATUS_ACTIVE => self::STATUS_ACTIVE_LBL,
+	          self::STATUS_INACTIVE => self::STATUS_INACTIVE_LBL
+	  ];
+	}
+
+	public function __construct()
+	{
+	  parent::__construct();
+	  $this->setTableName('product')->setPrimaryKey('product_id');
 	}
 }
-
-?>
