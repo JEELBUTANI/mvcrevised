9679c67a1da7b97b479b211b91ced6578d74a2ba
JEELBUTANI <68411725+JEELBUTANI@users.noreply.github.com>
2023-05-05 14:22:56 +0530

    day 03-04

--- jeel-butani-mvc/Model/Category/Row.php
+++ jeel-butani-mvc/Model/Category/Row.php
@@ -2,7 +2,7 @@
 
 class Model_Category_Row extends Model_Core_Table_Row
 {
-	function __construct(argument)
+	function __construct()
 	{
 		parent::__construct();
 		$this->setTableClass('Model_Category');
