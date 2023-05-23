9ba25b05438c0dcea6f64487e426e87ad95a46d0
JEELBUTANI <68411725+JEELBUTANI@users.noreply.github.com>
2023-05-23 20:09:42 +0530

    Delete jeel-butani-mvc directory

--- jeel-butani-mvc/Model/Category/Collection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Model_Category_Collection extends Model_Core_Table_Collection
-{
-	
-}
-
-?>
--- jeel-butani-mvc/Model/Salesman/Address/Collection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Model_Salesman_Address_Collection extends Model_Core_Table_Collection
-{
-	
-}
-
-?>
